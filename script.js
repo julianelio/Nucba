4900beb621de50f8b8074945d84d35460f18a2e2
julianelio <julianelio89@outlook.es>
2021-10-21 13:34:52 -0300

    Guardado-one

---
+++ script.js
@@ -0,0 +1,8 @@
+var miSustantivo = "El perro";
+var miAdjetivo = "blanco";
+var miVerbo = "Corrio";
+var miAdverbio  = "a la casa";
+
+var palabraEnlblanco = miSustantivo + " " + miAdjetivo +" "+ miVerbo +" "+ miAdverbio + " " + "Lentamente."
+
+console.log(palabraEnlblanco);
